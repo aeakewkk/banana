d5701cd6fb42194f0bfdf2205049e5f52f115db4
arakewkk <itokinapp.it@gmail.com>
2022-05-05 13:28:42 +0700

    add JSON

--- demo.py
+++ demo.py
@@ -1,3 +1,14 @@
 print("HI!")
 print("Coffee time.")
 print("Break time.")
+
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
